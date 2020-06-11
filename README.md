8cd9628d7ef68635774da5bc5ec513d0a3b0b6b1
Klaus Leggewie <k.leggewie@syzygy.de>
2020-06-11 19:21:29 +0200

    initial script

---
+++ README.md
@@ -0,0 +1,39 @@
+# ScheduledTasksReport
+
+PowerShell script to generate a CSV report of scheduled tasks (Windows). Uses the Get-ScheduledTask cmdlet.
+The report is for documentation of tasks withs basic task propoerties.
+
+## Prerequisites
+
+PowerShell Version >= 5.1 required.
+Tested with PowerShell 7.0 and 5.1 (Windows 10 Pro)
+
+## Input
+
+* $taskPath : task folder(s) to scan
+* $outcsv: path of CSV file
+
+## Output
+
+* Name
+* Path
+* Description
+* Status
+* Command
+* Arguments
+* ScheduleEnabled
+* StartDate
+* EndDate
+* StartTime
+* Interval
+* DaysOfWeek
+* Months
+* DaysOfMonth
+* Repetition
+* Duration
+  
+Not all possible definitions of a task are covered, e. g. if there are multiple triggers per task.
+
+## Known Issues
+
+The Get-ScheduledTask cmdlet does fetch MonthlyTriggers correctly. Therefore this interval is reported as "Other" (instead of "Monthly).
---
+++ scheduled-task-report-partos.ps1
@@ -0,0 +1,117 @@
+# Create CSV Report of scheduled Tasks (for Windows)
+# -------------------------------------------
+# take care of PS version. $PSVersionTable.PSVersion
+# this script is done for Version >= 5.1
+# basic example: https://devblogs.microsoft.com/scripting/weekend-scripter-use-powershell-to-document-scheduled-tasks/
+
+# pattern for task to fetch
+$taskPath = "\Microsoft\*"
+#name of the output file
+$outcsv = "c:\temp\My Scheduled Tasks-$(Get-Date -Format "yyy-MM-dd").csv"
+
+
+[Flags()] enum WeekDayFlag
+{
+Sun = 1
+Mon = 2
+Tue = 4
+Wed = 8
+Thu = 16
+Fri = 32
+Sat = 64
+}
+
+# reduce filepath to filename
+function GetFileNameFromPath($path)
+{
+    $filename =""
+
+    if ($path -Match "\\") {
+        try {
+            $filename = [io.path]::GetFileName($path)
+        }
+        catch {
+            Write-Host "GetFileNameFromPath failed" 
+            Write-Host $path 
+        }
+    }
+    else {
+        $filename = $path
+    }
+    return $filename
+}
+
+
+Get-ScheduledTask -TaskPath $taskPath |
+# optional: use "-TaskName" for specific task
+
+    ForEach-Object { 
+        [pscustomobject]@{
+
+        Name = $_.TaskName
+        Path = $_.TaskPath
+        Description = $_.Description
+
+        # if needed:
+            #LastResult = $(($_ | Get-ScheduledTaskInfo).LastTaskResult)
+            #NextRun = $(($_ | Get-ScheduledTaskInfo).NextRunTime)
+
+        Status = $_.State 
+
+        #reduce exe-path to filename
+        Command = GetFileNameFromPath($_.Actions.execute)
+        Arguments = $_.Actions.Arguments 
+
+        ScheduleEnabled =   if (($_.Triggers.Enabled) -ne $null)
+                                {($_.Triggers.Enabled | Select-Object -first 1)}
+                            else 
+                                {"N/A"} 
+        
+        StartDate = if (($_.Triggers.count -gt 0) -and ($_.Triggers[0].StartBoundary -ne $null)) 
+                        {(Get-Date -Date ($_.Triggers[0].StartBoundary) -Format "d")}
+                    else 
+                        {"N/A"}
+          
+        EndDate =   if (($_.Triggers.EndBoundary) -ne $null) 
+                        {($_.Triggers.EndBoundary)}
+                    else 
+                        {"N/A"} 
+
+        StartTime = if (($_.Triggers.count -gt 0) -and ($_.Triggers[0].StartBoundary -ne $null)) 
+                        {(Get-Date -Date ($_.Triggers[0].StartBoundary) -Format "HH:mm:ss")}
+                    else 
+                        {"N/A"}
+
+        Interval =  if ($_.Triggers.count -eq 0) 
+                        {"On Demand"}
+                    elseif ($_.Triggers.DaysInterval -eq 1) 
+                        {"Daily"}
+                    elseif ($_.Triggers.WeeksInterval -eq 1)  
+                        {"Weekly"}
+                    elseif ($_.Triggers.ScheduleByMonth -eq 1) 
+                        {"Monthly"}
+                    else 
+                        {"Other"}
+          
+        # Triggers.DaysOfWeek is a bitwise mask
+        DaysOfWeek =    if (($_.Triggers.count -gt 0) -and ($_.Triggers[0].DaysOfWeek -ne $null))
+                            {[WeekDayFlag]($_.Triggers[0].DaysOfWeek)}
+                        else
+                            {"N/A"} 
+
+        #remark: output for month does not work, bug of the Get-ScheduledTask cmdlet
+        #https://docs.microsoft.com/en-us/windows/win32/taskschd/monthlytrigger
+        Months =    if (($_.Triggers.MonthsOfYear) -ne $null) 
+                        {($_.Triggers.MonthsOfYear)}     
+                    else 
+                        {"N/A"} 
+        DaysOfMonth =   if (($_.Triggers.DaysOfMonth) -ne $null)
+                            {($_.Triggers.DaysOfMonth)}
+                        else 
+                            {"N/A"} 
+        
+        Repetition = ($_.Triggers.Repetition | Select-Object -ExpandProperty interval)
+        Duration = ($_.Triggers.Repetition | Select-Object -ExpandProperty duration)     
+
+        }
+     } | Export-Csv -Path $outcsv -NoTypeInformation
